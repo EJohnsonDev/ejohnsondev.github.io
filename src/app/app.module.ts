91cdac3d29597acf3733102a261d51b52f887c7a
Yakamoose <32146660+Yakamoose@users.noreply.github.com>
2022-12-05 21:10:26 -0800

    set up scss variables, overrides and fonts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleInfoModule } from './shared/components/title-info/title-info.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TitleInfoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
